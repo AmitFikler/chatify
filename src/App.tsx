b4ededa29b0ec585e8c3f8c8fd5b31fdf5d9df4e
AmitFikler <amitfikler@gmail.com>
2021-12-29 16:01:14 +0200

    🛠 build basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,83 @@
+import { useEffect, useRef, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { TextField } from '@material-ui/core';
+import { nanoid } from 'nanoid';
 
-import Button from '@mui/material/Button';
-import { useEffect, useRef, useState } from 'react';
 import {
   ClientToServerEvents,
   Message,
   ServerToClientEvents,
 } from './@types/types';
-function App() {
-  const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-    'http://localhost:4000'
-  ); // check
 
+function App() {
   // useStates
-  const [message, setMessage] = useState<Message>({ message: '', name: '' }); // Message
+  const [message, setMessage] = useState<Message>({ message: '', name: '' }); // Message state
   const [chat, setChat] = useState<Message[]>([]); // chat "history"
 
+  const socketRef =
+    useRef<Socket<ServerToClientEvents, ClientToServerEvents>>();
+
   useEffect(() => {
-    socket.on('connect', () => {
-      socket.on('replayMessage', ({ name, message }) => {
-        setChat((prevState) => {
-          return [...prevState, { name, message }];
-        });
+    socketRef.current = io('http://localhost:4000'); // connecting to 'http://localhost:4000'
+    socketRef.current.on('replayMessage', ({ name, message }) => {
+      setChat((prevState) => {
+        return [...prevState, { name, message }];
       });
     });
-
     // return () => socketRef.current.disconnect();
   }, []);
 
+  const onTextChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setMessage({ ...message, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { name, message }: { name: string; message: string } = message;
+    if (socketRef.current) {
+      socketRef.current.emit('message', { name, message });
+      setMessage({ message: '', name });
+    }
+  };
+
+  const renderChat = () => {
+    return chat.map(({ name, message }) => (
+      <div key={nanoid()}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
   return (
     <div className="App">
-      <h1> hello world</h1>
-      <TextField
-        required
-        id="outlined-required"
-        label="Required"
-        defaultValue="Hello World"
-      />
+      <form style={{ padding: '15px' }} onSubmit={onMessageSubmit}>
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="name"
+            onChange={(e) => onTextChange(e)}
+            defaultValue="name"
+            name="name"
+          />
+        </div>
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="message"
+            onChange={(e) => onTextChange(e)}
+            defaultValue="Hello World"
+            name="message"
+          />
+        </div>
+        <button>Send</button>
+      </form>
+      <div className="chat-data">{renderChat()}</div>
     </div>
   );
 }
