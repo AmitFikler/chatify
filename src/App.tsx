576a334cec8f70c92c63f25ebc818b66031b9952
AmitFikler <amitfikler@gmail.com>
2021-12-29 12:47:58 +0200

    📲 initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
