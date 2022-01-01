bc0d4c4c9a033789e2f7b064d64f063489a4ef50
AmitFikler <amitfikler@gmail.com>
2022-01-01 14:59:04 +0200

    🖌️ styling messages

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,13 +2,20 @@ import { nanoid } from 'nanoid';
 import { useAppSelector } from '../app/hooks';
 
 function Chat({ typingUser }: { typingUser: string }) {
-  const chat = useAppSelector((state) => state.chat.chat);
+  const { chat, username } = useAppSelector((state) => state.chat);
   const renderChat = () => {
     return chat.map(({ name, message }) => (
       <div key={nanoid()}>
-        <h3>
+        <div
+          className="message"
+          style={
+            name === username
+              ? { backgroundColor: 'rgba(69, 245, 192, 0.637)', float: 'right' }
+              : { backgroundColor: 'rgba(26, 243, 91, 0.637)' }
+          }
+        >
           {name}: <span>{message}</span>
-        </h3>
+        </div>
       </div>
     ));
   };
